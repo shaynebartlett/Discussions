8fd028533ce179933017e4ee563df39d19dc3d60
Achim Fischer <achim@codingfish.com>
2012-01-28 12:12:16 +0100

    Added messages inbox/outbox tables to uninstall script

--- admin/uninstall.mysql.sql
+++ admin/uninstall.mysql.sql
@@ -2,6 +2,10 @@ DROP TABLE if exists `#__discussions_categories`;
 
 DROP TABLE if exists `#__discussions_messages`;
 
+DROP TABLE if exists `#__discussions_messages_inbox`;
+
+DROP TABLE if exists `#__discussions_messages_outbox`;
+
 DROP TABLE if exists `#__discussions_meta`;
 
 DROP TABLE if exists `#__discussions_users`;
