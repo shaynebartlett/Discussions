d2f637322e9041bb5deb16f6c7b0d3f98f13929f
Achim Fischer <achim@codingfish.com>
2012-08-02 17:47:59 +0200

    Added comments table to uninstall script

--- admin/uninstall.mysql.sql
+++ admin/uninstall.mysql.sql
@@ -1,5 +1,9 @@
 DROP TABLE if exists `#__discussions_categories`;
 
+DROP TABLE if exists `#__discussions_configuration`;
+
+DROP TABLE if exists `#__discussions_comments`;
+
 DROP TABLE if exists `#__discussions_messages`;
 
 DROP TABLE if exists `#__discussions_messages_inbox`;
@@ -9,7 +13,5 @@ DROP TABLE if exists `#__discussions_messages_outbox`;
 DROP TABLE if exists `#__discussions_meta`;
 
 DROP TABLE if exists `#__discussions_users`;
-
-DROP TABLE if exists `#__discussions_configuration`;
 
 DROP TABLE if exists `#__discussions_flickr_cache`;
